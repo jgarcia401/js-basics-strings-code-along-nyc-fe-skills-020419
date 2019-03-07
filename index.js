59540879ddd1ae06b0ec8c6d95f3a201e9f9f00d
resourceful-transpiler-3397 <jgarcia401@gmail.com>
2019-03-07 01:25:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 
-const currentUser = ('Grace Hopper')
+const currentUser = 'Grace Hopper'
 
-const welcomeMessage = ('Welcome to Flatbook,')
+const welcomeMessage = 'Welcome to Flatbook,';
 
